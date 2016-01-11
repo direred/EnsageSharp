7d44151f2fc48446a5cbc864793c6cc536169c11
direred <dire_red@hotmail.com>
2016-01-11 13:01:34 -0800

    Update AssemblyInfo.cs

--- Item Panel/Properties/AssemblyInfo.cs
+++ Item Panel/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("ItemPanel")]
+[assembly: AssemblyTitle("Powered by Dire_red")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ItemPanel")]
+[assembly: AssemblyProduct("Powered bye Dire_red")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
